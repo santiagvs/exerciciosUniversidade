d4712b33f09930138e4ea1ac5f1ed97ce8a2778a
Santiago <victor.lacsilva0102@gmail.com>
2023-08-05 20:59:21 -0300

    commit do app locadora

---
+++ locadora/locadora.py
@@ -0,0 +1,31 @@
+import metodosLocadora as app
+
+clientes = []
+
+while True:
+  app.cadastro_geral(clientes)
+
+  print("\nValor total pago por placa:")
+  for cliente in clientes:
+    print(f"\n{cliente['placa']}: R${cliente['valor_total']:.2f}")
+
+  km_total = sum(cliente['km'] for cliente in clientes)
+  numero_clientes = len(clientes)
+  media_km_contratados = km_total / numero_clientes
+  print(f"Média de quilomêtros contratados: {media_km_contratados}")
+
+  print("\nClientes de sexo feminino que fecharam aluguéis acima de 7 dias contratados:")
+  for cliente in clientes:
+    if cliente["sexo"] == "F" and cliente["dias"] > 7:
+      print(f"{cliente['nome']}")
+    else:
+      print("Não há registros.")
+
+  # generator expression
+  # print(f"{cliente['nome']}" for cliente in clientes if cliente["sexo"] == "F" and cliente["dias"] > 7)
+
+  if input("\nDeseja sair do programa? (s/n) ") != 's':
+    continue
+  else:
+    print("Obrigado por utilizar!")
+    break
---
+++ locadora/metodosLocadora.py
@@ -0,0 +1,36 @@
+import os
+
+TAXA_DIARIA = 70
+TAXA_POR_KM = 0.10
+
+def cadastro_geral(clientes):
+  while True:
+    os.system('clear')
+    nome_do_cliente = input("Digite aqui o nome do cliente: ")
+    sexo = cadastro_sexo()
+    placa_do_carro = input("Digite aqui a placa do carro: ")
+    qtd_km_contratados = int(input("Digite a quantidade de km contratados: "))
+    qtd_dias_contratados = int(input("Digite a quantidade de dias contratados: "))
+
+    cliente = {
+      "nome": nome_do_cliente,
+      "sexo": sexo,
+      "placa": placa_do_carro,
+      "km": qtd_km_contratados,
+      "dias": qtd_dias_contratados,
+      "valor_total": 0
+    }
+
+    cliente["valor_total"] = TAXA_DIARIA * cliente["dias"] + TAXA_POR_KM * cliente["km"]
+
+    clientes.append(cliente)
+
+    if input("Deseja inserir mais dados? Digite 's' se sim, e 'n' se não: ") != 's':
+      break
+
+def cadastro_sexo():
+  sexo = input("Digite F para *feminino* e M para *masculino*: ")
+  while True:
+    if sexo in ["F", "M"]:
+      break
+    print("Por favor, digite o sexo de acordo com os caracteres F ou M.")
